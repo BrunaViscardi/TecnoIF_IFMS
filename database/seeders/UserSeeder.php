e2f9484b68cbee56f1bdc56f33ba7ef2ad146324
Bruna Viscardi <bruna.viscardi@estudante.ifms.edu.br>
2021-02-26 15:57:53 -0300

    finalização e mudança para o heroku

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,43 +16,10 @@ class UserSeeder extends Seeder
     public function run()
     {
 
-           $id = DB::table('mentorados')->insert([
-               'nome' => 'Mentorado',
-           'data_nascimento' => '2003-03-02' ,
-            'telefone' => '(99)99999-9999',
-           'cpf'=>'063.961.211-31' ,
-           'rg' => '3333',
-           'endereco'=> 'bla bla',
-           'email' =>'Mentorado@gmail.com',
-           'campus'=>'Jardim',
-
-       ]
-       );
-
-        User::create(
-        [
-            'name' => 'Mentorado',
-            'email' => 'Mentorado@gmail.com',
-            'password' => Hash::make('senha123'),
-            'role'=> 0,
-           'mentorado_id'=> $id
-        ]
-
-        );
         User::create(
             [
-                'name' => 'Gestor',
-                'email' => 'equipeGestora@gmail.com',
-                'password' => Hash::make('senha123'),
-                'role'=> 1,
-
-            ]
-
-        );
-        User::create(
-            [
-                'name' => 'Coordenador',
-                'email' => 'coordenador@gmail.com',
+                'name' => 'Ivilaine delguingaro',
+                'email' => 'ivilaine.delguingaro@ifms.edu.br',
                 'password' => Hash::make('senha123'),
                 'role'=> 2,
 
